ba49cef9b50f2c3b75e19f76b1b17446c95db0c2
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-01-11 09:22:21 -0600

    Adding theme config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '@/theme'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,7 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppRouterCacheProvider >{children}</AppRouterCacheProvider>
+        <AppRouterCacheProvider >
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   )
