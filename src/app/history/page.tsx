f9ba770e99b65d9aad3c2e5640bab95dc4b651ab
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-01-11 15:25:50 -0600

    Adding a comment

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,50 +1,57 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Divider, List, ListItem, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  Typography,
+} from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import HistoryItem from "./HistoryItem";
+import HistoryItem from './HistoryItem';
 
 export type HistoryRow = {
-    prompt: string,
-    date: Date|string,
+  prompt: string;
+  date: Date | string;
 };
 async function getHistoryData() {
-    // Fetching with a 2 seconds delay to test loading state
-    await fetch("https://postman-echo.com/delay/2", {
-        cache: 'no-cache',
-    });
-    // Here we query DB
-    return [
-        {
-            prompt: 'Little family in a camp with a cup',
-            date: 'Yesterday',
-        },
-        {
-            prompt: 'Little family in a camp with a cup',
-            date: 'Yesterday',
-        },
-        {
-            prompt: 'Little family in a camp with a cup',
-            date: 'Yesterday',
-        },
-    ];
-
+  // Fetching with a 2 seconds delay to test loading state
+  await fetch('https://postman-echo.com/delay/2', {
+    cache: 'no-cache',
+  });
+  // Here we query DB
+  return [
+    {
+      prompt: 'Little family in a camp with a cup',
+      date: 'Yesterday',
+    },
+    {
+      prompt: 'Little family in a camp with a cup',
+      date: 'Yesterday',
+    },
+    {
+      prompt: 'Little family in a camp with a cup',
+      date: 'Yesterday',
+    },
+  ];
 }
 
-
 export default async function Page() {
-    const historyData = await getHistoryData();
-    return <Accordion>
-        <AccordionSummary
-        expandIcon={<ExpandMoreIcon/>}
-        >
-            History
-        </AccordionSummary>
-        <AccordionDetails>
-            <List>
-                {
-                    historyData.map((h, i) => (<HistoryItem history={h} key={i}/>))
-                }
-            </List>
-        </AccordionDetails>
-
+  const historyData = await getHistoryData();
+  return (
+    <Accordion>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        {/* TODO: internationalization */}
+        History
+      </AccordionSummary>
+      <AccordionDetails>
+        <List>
+          {historyData.map((h, i) => (
+            <HistoryItem history={h} key={i} />
+          ))}
+        </List>
+      </AccordionDetails>
     </Accordion>
+  );
 }
