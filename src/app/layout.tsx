40da8c675fef48db3cde9683def2db62a3444bd7
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-01-11 09:12:34 -0600

    Adding MUI configs with sample page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider >{children}</AppRouterCacheProvider>
+      </body>
     </html>
   )
 }
