a8836d53ad80e11e0e8772cd5d12198027d00ef3
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-01-12 16:06:30 -0300

    Add locale param to root layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,11 +14,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params: { locale },
 }: {
   children: React.ReactNode;
+  params: { locale: string };
 }) {
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={inter.className}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
