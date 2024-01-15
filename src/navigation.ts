24ad016ed0b0157be36f8d54891acda2c992ec8e
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-01-15 14:54:49 -0300

    Add explain comment

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,3 +1,11 @@
+/**
+ * This file exports the wrapped versions of `Link`, `redirect`, `usePathname`, and `useRouter` functions.
+ * They provide support for internationalization (i18n)
+ * by automatically adding the locale prefix to the pathnames.
+ * For more information on how to use these functions, refer to the documentation
+ * at https://next-intl-docs.vercel.app/docs/routing/navigation#apis.
+ **/
+
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
 import { supportedLocales, localePrefix } from './intl/intl.constants';
