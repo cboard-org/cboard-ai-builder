4bfcde0fad35401682571838e5e9248c6e5822c1
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-01-29 20:53:04 -0300

    Implement Grid structure

--- src/app/[locale]/dashboard/@board/Grid/Grid.tsx
+++ src/app/[locale]/dashboard/@board/Grid/Grid.tsx
@@ -2,9 +2,14 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import GridBase from './GridBase';
 import styles from './Grid.module.css';
 import { GridProps } from './GridTypes';
+import * as utils from './utils';
+import Row from './Row/Row';
+import DroppableCell from './DroppableCell/DroppableCell';
+import DraggableItem from './DraggableItem/DraggableItem';
+
+const page = 0;
 
 function Grid({
   items,
@@ -18,26 +23,52 @@ function Grid({
   className,
   style,
 }: GridProps) {
-  const gridClassName = classNames(styles.grid, className);
+  const gridClassName = classNames(styles.Grid, className);
+
+  const grid = utils.sortGrid({ columns, rows, order, items });
+
+  let itemIndex = 0;
 
   return (
     <div
-      className={styles.root}
+      className={styles.GridContainer}
       style={style}
       //onKeyDown={handleOnKeyDown}
     >
-      <GridBase
-        className={gridClassName}
-        items={items}
-        page={0}
-        columns={columns}
-        dragAndDropEnabled={dragAndDropEnabled}
-        onItemDrop={onItemDrop}
-        order={order}
-        renderEmptyCell={renderEmptyCell}
-        renderItem={renderItem}
-        rows={rows}
-      />
+      <div className={gridClassName}>
+        {grid.map((row, rowIndex) => (
+          <Row key={rowIndex.toString()}>
+            {row.map((item, columnIndex) => {
+              const yPosition = page * rows + rowIndex;
+              const idWithPosition = `${columnIndex}-${yPosition}`;
+              return (
+                <DroppableCell
+                  key={columnIndex.toString()}
+                  id={idWithPosition}
+                  accept={'grid-item'}
+                  onDrop={(item) => {
+                    const position = { row: rowIndex, column: columnIndex };
+
+                    onItemDrop(item, position);
+                  }}
+                >
+                  {item ? (
+                    <DraggableItem
+                      type={'grid-item'}
+                      id={item.id}
+                      disabled={!dragAndDropEnabled}
+                    >
+                      {renderItem(item, itemIndex++)}
+                    </DraggableItem>
+                  ) : (
+                    renderEmptyCell && renderEmptyCell()
+                  )}
+                </DroppableCell>
+              );
+            })}
+          </Row>
+        ))}
+      </div>
     </div>
   );
 }
