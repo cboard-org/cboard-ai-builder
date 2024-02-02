ffb00c7e3e5f64d912f8b13de1c717122ba74f51
tomivm <tomivm98@gmail.com>
2024-02-02 16:38:21 -0300

    Fix styles on history

--- src/app/[locale]/dashboard/@history/HistoryItem.tsx
+++ src/app/[locale]/dashboard/@history/HistoryItem.tsx
@@ -6,6 +6,8 @@ import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
 import ListItemText from '@mui/material/ListItemText';
 import { HistoryRow, removeHistory } from './actions';
 import { useFormatter } from 'next-intl';
+import Box from '@mui/material/Box';
+
 type Props = {
   history: HistoryRow;
   onDelete: (history: HistoryRow) => void;
@@ -19,19 +21,24 @@ export default function HistoryItem({ history, onDelete }: Props) {
   };
   return (
     <>
-      <ListItem disableGutters divider>
+      <ListItem
+        divider
+        secondaryAction={
+          <Box>
+            <IconButton size="small">
+              <EditOutlined fontSize="small" />
+            </IconButton>
+            <IconButton onClick={deleteHistory} size="small">
+              <DeleteOutline fontSize="small" />
+            </IconButton>
+          </Box>
+        }
+      >
         <ListItemText
           primary={history.prompt}
           secondary={format.relativeTime(new Date(history.date))}
+          sx={{ pr: 4 }}
         />
-        <ListItemSecondaryAction>
-          <IconButton size="small">
-            <EditOutlined fontSize="small" />
-          </IconButton>
-          <IconButton onClick={deleteHistory} size="small">
-            <DeleteOutline fontSize="small" />
-          </IconButton>
-        </ListItemSecondaryAction>
       </ListItem>
     </>
   );
