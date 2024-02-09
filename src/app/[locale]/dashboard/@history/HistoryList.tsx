da4b28a94bac08828d50e2dee8e2eb38588c3cac
tomivm <tomivm98@gmail.com>
2024-02-09 13:29:03 -0300

    implement Pagination on HistoryList

--- src/app/[locale]/dashboard/@history/HistoryList.tsx
+++ src/app/[locale]/dashboard/@history/HistoryList.tsx
@@ -1,10 +1,25 @@
 'use client';
 
+import { useState } from 'react';
+import Stack from '@mui/material/Stack';
 import List from '@mui/material/List';
+import Pagination from '@mui/material/Pagination';
 import { useOptimistic } from 'react';
 import DataItem from '@/components/DataItem/DataItem';
 import { removeHistoryData } from './actions';
 import { HistoryData } from './actions';
+
+const sxStyles = {
+  stack: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  list: { width: '100%' },
+  pagination: { paddingBottom: 1 },
+};
 
 export default function HistoryList({
   initialHistories,
@@ -22,11 +37,30 @@ export default function HistoryList({
     await removeHistoryData(historyToDelete);
   };
 
+  const [page, setPage] = useState(1);
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+  const itemsPerPage = 2;
+  const paginationCount = Math.ceil(histories.length / itemsPerPage);
+  const start = (page - 1) * itemsPerPage;
+  const truncatedItems = histories.slice(start, start + itemsPerPage);
+
   return (
-    <List>
-      {histories.map((data, index) => (
-        <DataItem data={data} key={index} onDelete={deleteHistoryData} />
-      ))}
-    </List>
+    <Stack sx={sxStyles.stack}>
+      <List sx={sxStyles.list}>
+        {truncatedItems.map((data, index) => (
+          <DataItem data={data} key={index} onDelete={deleteHistoryData} />
+        ))}
+      </List>
+      <Pagination
+        sx={sxStyles.pagination}
+        count={paginationCount}
+        color="primary"
+        siblingCount={0}
+        page={page}
+        onChange={handleChange}
+      />
+    </Stack>
   );
 }
