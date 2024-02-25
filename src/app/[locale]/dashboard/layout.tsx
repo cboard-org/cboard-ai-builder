fc78884c622d62e1604b75310d2f4248d8dd256a
tomivm <tomivm98@gmail.com>
2024-02-25 12:47:15 -0300

    Use lg breakpoint to change columns on layout

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -14,7 +14,7 @@ const sxStyles = {
     overflowY: { xs: 'scroll', sm: 'unset' },
     alignContent: { xs: 'flex-start', sm: 'inherit' },
     flexDirection: { xs: 'column' },
-    gridTemplateColumns: { xs: '1fr', sm: '1fr 2fr', md: '1fr 3fr' },
+    gridTemplateColumns: { xs: '1fr', sm: '1fr 2fr', lg: '1fr 3fr' },
     gridTemplateRows: {
       xs: `max-content max-content calc(100% - ${menuBarHeight}px);`,
       sm: 'auto 1fr',
@@ -27,8 +27,8 @@ const sxStyles = {
       sm: `"title board"
   "sidebar board"`,
     },
-    columnGap: { xs: 0, sm: smSpacing, md: mdSpacing },
-    rowGap: { xs: xsSpacing, sm: smSpacing, md: mdSpacing },
+    columnGap: { xs: 0, sm: smSpacing, lg: mdSpacing },
+    rowGap: { xs: xsSpacing, sm: smSpacing, lg: mdSpacing },
   },
   sidebar: {
     display: 'flex',
@@ -47,9 +47,9 @@ export default function Dashboard(props: {
   board: React.ReactNode;
 }) {
   return (
-    <Box py={{ xs: 0, sm: 2, md: 3 }} className={styles.dashboardBox}>
+    <Box py={{ xs: 0, sm: 2, lg: 3 }} className={styles.dashboardBox}>
       <Box
-        px={{ xs: 2, md: 4 }}
+        px={{ xs: 2, lg: 4 }}
         sx={sxStyles.dashboardContainer}
         className={styles.dashboardContainer}
       >
