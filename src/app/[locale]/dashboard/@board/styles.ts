c4cdfe49c56dffdf4a580eb2313cbcac062cc6b4
tomivm <tomivm98@gmail.com>
2024-02-27 21:38:38 -0300

    rename styles file

--- src/app/[locale]/dashboard/@board/styles.ts
+++ src/app/[locale]/dashboard/@board/styles.ts
@@ -3,11 +3,13 @@ import { SxProps } from '@mui/system/styleFunctionSx/styleFunctionSx';
 export default {
   exampleButton: {
     width: '100%',
+    p: 0,
   },
   textArea: {
     backgroundColor: 'white',
-    fontSize: '0.5rem',
     color: 'black',
     pointerEvents: 'none',
+    width: '100%',
+    maxWidth: '250px',
   },
 } satisfies Record<string, SxProps>;
