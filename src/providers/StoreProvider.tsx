1dc00c4e335ac13b8ffded13bf250cc588eb9fa9
tomivm <tomivm98@gmail.com>
2024-03-03 16:53:41 -0300

    Adapt The Store Provider to use Zustand Slices Pattern
    https://docs.pmnd.rs/zustand/guides/slices-pattern

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -1,15 +1,39 @@
-import ComposeComponents from '@/utils/combineComponents';
-import { PromptStoreProvider } from './PromptStoreProvider';
-import { BoardStoreProvider } from './BoardStoreProvider';
+'use client';
+import { createContext, useContext, useRef } from 'react';
+import { StoreApi, createStore, useStore } from 'zustand';
+import { PromptSlice, createPromptSlice } from '@/stores/prompt-slice';
+import { BoardSlice, createBoardSlice } from '@/stores/board-slice';
+import { devtools } from 'zustand/middleware';
 
-export default function StoreProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export type Store = PromptSlice & BoardSlice;
+export const StoreContext = createContext<StoreApi<Store> | null>(null);
+
+export default function StoreProvider({ children }: React.PropsWithChildren) {
+  const storeRef = useRef<StoreApi<Store>>();
+  if (!storeRef.current) {
+    storeRef.current = createStore<Store>()(
+      devtools((...props) => ({
+        ...createPromptSlice(...props),
+        ...createBoardSlice(...props),
+      })),
+    );
+  }
+
   return (
-    <ComposeComponents components={[PromptStoreProvider, BoardStoreProvider]}>
+    <StoreContext.Provider value={storeRef.current}>
       {children}
-    </ComposeComponents>
+    </StoreContext.Provider>
   );
 }
+
+export const useBoundStore = <StoreProps,>(
+  selector: (store: Store) => StoreProps,
+): StoreProps => {
+  const storeContext = useContext(StoreContext);
+
+  if (!storeContext) {
+    throw new Error(`useStore must be use within StoreProvider`);
+  }
+
+  return useStore(storeContext, selector);
+};
