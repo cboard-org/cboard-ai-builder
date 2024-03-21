148093f51d0c5eb135a9e4dccfad378995ae4ede
tomivm <tomivm98@gmail.com>
2024-03-21 20:49:57 -0300

    useSetInitialBoardHook on client Board Container

--- src/app/[locale]/dashboard/[id]/@board/BoardPage.tsx
+++ src/app/[locale]/dashboard/[id]/@board/BoardPage.tsx
@@ -2,9 +2,29 @@
 import InitialContent from './InitialContent';
 import BoardContainer from './BoardContainer';
 import { useBoundStore } from '@/providers/StoreProvider';
+import { useEffect } from 'react';
+import { BoardRecord } from '@/commonTypes/Board';
 
-export default function BoardPage() {
+type RemoteInitialBoard = BoardRecord | null;
+
+const useSetInitialBoard = (remoteInitialBoard: RemoteInitialBoard) => {
+  const { setBoard, stashedDashboard } = useBoundStore((state) => state);
+
+  useEffect(() => {
+    if (remoteInitialBoard) {
+      return setBoard(remoteInitialBoard);
+    }
+    if (stashedDashboard.board) return setBoard(stashedDashboard.board);
+  }, [remoteInitialBoard, setBoard, , stashedDashboard]);
+};
+
+export default function BoardPage({
+  remoteInitialBoard,
+}: {
+  remoteInitialBoard: RemoteInitialBoard;
+}) {
   const { shouldDisplayInitialContent } = useBoundStore((state) => state);
+  useSetInitialBoard(remoteInitialBoard);
 
   return shouldDisplayInitialContent ? <InitialContent /> : <BoardContainer />;
 }
