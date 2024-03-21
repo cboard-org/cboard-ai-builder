eafcb0457ba1511958d5769f6a684879ee5fb29c
tomivm <tomivm98@gmail.com>
2024-03-21 20:21:29 -0300

    Create stuff to stash a Dashboard

--- src/stores/dashboard-slice.ts
+++ src/stores/dashboard-slice.ts
@@ -1,17 +1,33 @@
 import { StateCreator } from 'zustand';
 import { Store } from './../providers/StoreProvider';
+import { PromptRecord } from '@/commonTypes/Prompt';
+import { BoardRecord } from '@/commonTypes/Board';
 
 export type DashboardStoreRecord = {
   isGenerationPending: boolean;
+  dashboardId: string;
+  stashedDashboard: {
+    id: string;
+    prompt: PromptRecord | null;
+    board: BoardRecord | null;
+  };
 };
 
 export type DashboardActions = {
   setGenerationPending: (pending: boolean) => void;
+  setDashboardId: (id: string) => void;
+  stashDashboard: () => void;
 };
 export type DashboardSlice = DashboardStoreRecord & DashboardActions;
 
 export const defaultDashboardState: DashboardStoreRecord = {
   isGenerationPending: false,
+  dashboardId: 'create',
+  stashedDashboard: {
+    id: 'new',
+    prompt: null,
+    board: null,
+  },
 };
 
 export const createDashboardSlice: StateCreator<
@@ -26,5 +42,22 @@ export const createDashboardSlice: StateCreator<
       type: 'Dashboard/setPending',
       pending,
     });
+  },
+  setDashboardId: (id: string) => {
+    set(() => ({ dashboardId: id }), false, {
+      type: 'Dashboard/setId',
+      id,
+    });
+  },
+  stashDashboard: () => {
+    set(
+      ({ prompt, board }) => ({
+        stashedDashboard: { id: 'new', prompt, board },
+      }),
+      false,
+      {
+        type: 'Dashboard/stash',
+      },
+    );
   },
 });
