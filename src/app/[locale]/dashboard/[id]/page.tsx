51a1c51b0c98cd74836b6c7683ae2ee8f9c3c81f
tomivm <tomivm98@gmail.com>
2024-03-23 00:14:50 -0300

    detect changes on dashboard Id in dashboard page

--- src/app/[locale]/dashboard/[id]/page.tsx
+++ src/app/[locale]/dashboard/[id]/page.tsx
@@ -1,3 +1,42 @@
-export default function DashboardPage() {
+'use client';
+import { useBoundStore } from '@/providers/StoreProvider';
+import { useEffect, useState } from 'react';
+
+const useOnLocationPathnameChangeHandler = ({}) => {
+  const [id, setId] = useState('');
+  useEffect(() => {
+    window.addEventListener('popstate', () => {
+      const splited = document.location.pathname.split('/');
+      setId(splited[splited.length - 1]);
+    });
+    return () => {
+      window.removeEventListener('popstate', () => {});
+    };
+  }, []);
+  return id;
+};
+
+const useSetDashboardIdOnChange = ({ idParam }: { idParam: string }) => {
+  const { setDashboardId } = useBoundStore((state) => state);
+  const idFromPathnameChangeHandler = useOnLocationPathnameChangeHandler({});
+
+  useEffect(() => {
+    setDashboardId(idParam);
+  }, [idParam, setDashboardId]);
+
+  useEffect(() => {
+    if (idFromPathnameChangeHandler.length > 0) {
+      setDashboardId(idFromPathnameChangeHandler);
+    }
+  }, [idFromPathnameChangeHandler, setDashboardId]);
+};
+
+export default function DashboardPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  useSetDashboardIdOnChange({ idParam: id });
+
   return <></>;
 }
