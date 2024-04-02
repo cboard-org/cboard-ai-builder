cb53e5f1dca231a753c3f6ff9768725e8447040f
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-04-02 15:52:14 -0300

    Refactor History component to be server component

--- src/app/[locale]/dashboard/[id]/history/History.tsx
+++ src/app/[locale]/dashboard/[id]/history/History.tsx
@@ -1,27 +1,7 @@
-'use client';
+import { getPromptHistoryData } from './actions';
+import HistoryList from './HistoryList';
 
-import { removeHistoryData } from './actions';
-import { HistoryData } from './actions';
-import DataList from '@/components/DataList/DataList';
-import { useTransition } from 'react';
-
-export default function History({
-  initialData: histories,
-}: {
-  initialData: HistoryData[];
-}) {
-  const [isPending, startTransition] = useTransition();
-
-  const deleteHistoryData = async (historyToDelete: HistoryData) => {
-    startTransition(async () => {
-      await removeHistoryData(historyToDelete.id);
-    });
-  };
-
-  return (
-    <DataList<HistoryData>
-      list={histories}
-      deleteItem={{ deleteData: deleteHistoryData, isDeleting: isPending }}
-    />
-  );
+export default async function History() {
+  const initialData = await getPromptHistoryData();
+  return <HistoryList initialData={initialData} />;
 }
