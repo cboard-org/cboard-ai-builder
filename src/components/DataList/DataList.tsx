59d0a58ec7a511482448b5f52562e917cd9d7717
tomivm <tomivm98@gmail.com>
2024-04-04 15:57:55 -0300

    Preserve page on SavedData between navigation

--- src/components/DataList/DataList.tsx
+++ src/components/DataList/DataList.tsx
@@ -7,6 +7,8 @@ import Pagination from '@mui/material/Pagination';
 import DataItem from '@/components/DataItem/DataItem';
 import { BaseDataItemType } from '@/components/DataItem/DataItem';
 import styles from './styles';
+import { usePathname, useRouter } from '@/navigation';
+import { useSearchParams } from 'next/navigation';
 
 const ITEMS_PER_PAGE = 2;
 
@@ -20,9 +22,15 @@ export default function DataList<DataItemType extends BaseDataItemType>({
     isDeleting: boolean;
   };
 }) {
-  const [page, setPage] = useState(1);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [page, setPage] = useState(parseInt(searchParams.get('page') ?? '1'));
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
+    const newParams = new URLSearchParams(searchParams.toString());
+    newParams.set('page', value.toString());
+    router.replace(`${pathname}?${newParams.toString()}`);
   };
   const paginationCount = Math.ceil(list.length / ITEMS_PER_PAGE);
   const start = (page - 1) * ITEMS_PER_PAGE;
