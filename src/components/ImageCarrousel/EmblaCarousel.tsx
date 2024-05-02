f551e415666c377a0f77cb1d1b3d3769a335a13c
tomivm <tomivm98@gmail.com>
2024-05-02 13:58:23 -0300

    Clean carrousel

--- src/components/ImageCarrousel/EmblaCarousel.tsx
+++ src/components/ImageCarrousel/EmblaCarousel.tsx
@@ -1,17 +1,10 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { EmblaOptionsType } from 'embla-carousel';
 import useEmblaCarousel from 'embla-carousel-react';
 import { Thumb } from './EmblaCarouselThumbsButton';
 import './css/base.css';
 import './css/embla.css';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
-// import ReplayIcon from '@mui/icons-material/Replay';
-// import CircularProgress from '@mui/material/CircularProgress';
-// import GenerateButton from '../Symbol/GenerateButton';
-import Box from '@mui/material/Box';
-import Fade from '@mui/material/Fade';
 
 type PropType = {
   slides: number[];
@@ -54,33 +47,23 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
     emblaMainApi.scrollNext();
   };
 
-  const generatePictoKey = slides.length;
-  const slidesAndGeneratePicto = useMemo(() => {
-    return [...slides, generatePictoKey];
-  }, [generatePictoKey, slides]);
-  console.log('generatePictoKey', generatePictoKey);
-  const isGenerateSelected = generatePictoKey === selectedIndex;
-
   return (
     <div className="tile__container theme-light">
       <div className="embla" style={{ backgroundColor: props.tileColor }}>
         <div className="embla__viewport" ref={emblaMainRef}>
           <div className="embla__container">
-            {slidesAndGeneratePicto.map((index) => (
+            {slides.map((index) => (
               <div className="embla__slide embla__class-names" key={index}>
                 <div className="embla__tile" onClick={handleNextImage}>
-                  {index !== generatePictoKey ? (
-                    <img
-                      className="embla__slide__img"
-                      src={`https://api.arasaac.org/api/pictograms/1024${index}`}
-                      alt="slide.label"
-                    />
-                  ) : (
-                    <div className="embla__slide__empty-image"></div>
-                  )}
+                  <img
+                    className="embla__slide__img"
+                    src={`https://api.arasaac.org/api/pictograms/1024${index}`}
+                    alt="slide.label"
+                  />
                 </div>
               </div>
             ))}
+            {slides.length === 0 && <EmptyTile />}
           </div>
         </div>
       </div>
@@ -96,46 +79,14 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
       <div className="embla-thumbs">
         <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
           <div className="embla-thumbs__container">
-            {slidesAndGeneratePicto.map((index) =>
-              index !== generatePictoKey ? (
-                <Thumb
-                  key={index}
-                  onClick={() => onThumbClick(index)}
-                  selected={index === selectedIndex}
-                  index={index}
-                />
-              ) : (
-                <Box
-                  key={generatePictoKey}
-                  className="embla-thumbs__slide"
-                  sx={{
-                    // flex: '0 0 fit-content',
-                    minHeight: '48px',
-                  }}
-                >
-                  <Fade in={true}>
-                    {
-                      <Button
-                        sx={{
-                          display: 'flex',
-                          alignItems: 'space-between',
-                          width: '100%',
-                          // justifyContent: 'space-around',
-                          justifyContent: 'center',
-                          height: '100%',
-                        }}
-                        onClick={() => onThumbClick(generatePictoKey)}
-                        className="embla-thumbs__slide__number"
-                        variant={isGenerateSelected ? 'contained' : 'outlined'}
-                        // startIcon={<AutoAwesomeIcon />}
-                      >
-                        <AutoAwesomeIcon />
-                      </Button>
-                    }
-                  </Fade>
-                </Box>
-              ),
-            )}
+            {slides.map((index) => (
+              <Thumb
+                key={index}
+                onClick={() => onThumbClick(index)}
+                selected={index === selectedIndex}
+                index={index}
+              />
+            ))}
           </div>
         </div>
       </div>
@@ -143,4 +94,11 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
   );
 };
 
+const EmptyTile = () => (
+  <div className="embla__slide embla__class-names">
+    <div className="embla__tile">
+      <div className="embla__slide__empty-image"></div>
+    </div>
+  </div>
+);
 export default EmblaCarousel;
