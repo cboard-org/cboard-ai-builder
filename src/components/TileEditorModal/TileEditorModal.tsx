feacfe2c6b1cb8d64b3b19fcba1183add7fc7e3b
tomivm <tomivm98@gmail.com>
2024-07-08 11:57:33 +0200

    Improve tile editor dialog styles

--- src/components/TileEditorModal/TileEditorModal.tsx
+++ src/components/TileEditorModal/TileEditorModal.tsx
@@ -5,6 +5,7 @@ import TileEditor from './TileEditor';
 import Dialog from '@mui/material/Dialog';
 import { useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import styles from './styles';
 
 export default function TileEditorModal({
   onClose,
@@ -21,10 +22,16 @@ export default function TileEditorModal({
     <>
       <Dialog
         fullScreen={fullScreen}
+        maxWidth="sm"
         open={true}
         onClose={onClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
+        sx={{
+          '& .MuiDialog-paper': {
+            ...(fullScreen ? styles.dialogPaperFullScreen : styles.dialogPaper),
+          },
+        }}
       >
         <TileEditor onClose={onClose} initialTile={tile} />
       </Dialog>
--- src/components/TileEditorModal/styles.ts
+++ src/components/TileEditorModal/styles.ts
@@ -1,6 +1,21 @@
 import { SxProps } from '@mui/system/styleFunctionSx/styleFunctionSx';
 
 export default {
+  dialogPaperFullScreen: {
+    margin: 0,
+    maxHeight: '100%',
+    height: '100%',
+    width: '100%',
+    maxWidth: '100%',
+  },
+  dialogPaper: {
+    width: '90%',
+    maxWidth: '550px',
+    height: '85%',
+    maxHeight: '90%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
   tileControlsContainer: {
     display: 'flex',
     flexDirection: 'column',
