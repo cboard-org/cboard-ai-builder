7ee724a5c1a992ab4df62a0846e787e5c60e0fe7
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-07-30 12:43:37 -0300

    Rename and remove IconButton from GenerateIcon component

--- src/components/Symbol/GenerateIcon.tsx
+++ src/components/Symbol/GenerateIcon.tsx
@@ -1,13 +1,10 @@
-import IconButton from '@mui/material/IconButton';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import Tooltip from '@mui/material/Tooltip';
 
-export default function GenerateButton() {
+export default function GenerateIcon() {
   return (
     <Tooltip title={'Create a Pictogram'}>
-      <IconButton>
-        <AutoAwesomeIcon />
-      </IconButton>
+      <AutoAwesomeIcon />
     </Tooltip>
   );
 }
