ed4e05b3e6018ebb56f9c470e4061bf6acdbd79a
tomivm <tomivm98@gmail.com>
2024-07-30 18:10:11 +0200

    Clean the prompt on new Board click

--- src/app/[locale]/dashboard/[id]/NewBoardLink/NewBoardLink.tsx
+++ src/app/[locale]/dashboard/[id]/NewBoardLink/NewBoardLink.tsx
@@ -7,13 +7,15 @@ import IconButton from '@mui/material/IconButton';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import Tooltip from '@mui/material/Tooltip';
 import { useTranslations } from 'next-intl';
+import { useBoundStore } from '@/providers/StoreProvider';
 
 // type Props = {};
 
 function NewBoardLink() {
   const messages = useTranslations('Dashboard');
+  const cleanPrompt = useBoundStore((state) => state.cleanPrompt);
   return (
-    <Link href={`/dashboard/${INITIAL_CONTENT_ID}`}>
+    <Link onClick={cleanPrompt} href={`/dashboard/${INITIAL_CONTENT_ID}`}>
       <Box sx={styles.linkContent}>
         <Tooltip title={messages('newBoard')}>
           <IconButton>
