98175cf1f0d3732d00396e57cdb32f31a3e7cb95
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-07-30 17:13:11 -0300

    Upadte defaultMessages source on i18n

--- src/intl/i18n.ts
+++ src/intl/i18n.ts
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
-import { supportedLocales, defaultLocale } from './intl.constants';
+import { supportedLocales } from './intl.constants';
 import deepmerge from 'deepmerge';
 
 export default getRequestConfig(async ({ locale }) => {
@@ -8,8 +8,7 @@ export default getRequestConfig(async ({ locale }) => {
 
   const activeLocaleMessages = (await import(`./dictionaries/${locale}.json`))
     .default;
-  const defaultMessages = (await import(`./dictionaries/${defaultLocale}.json`))
-    .default;
+  const defaultMessages = (await import(`./cbuilder.json`)).default;
 
   return {
     messages: deepmerge(defaultMessages, activeLocaleMessages),
