f81ec79dced01d3c1ea3ef94483d468e0d28fb65
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-08-19 19:41:00 -0300

    Create Sidebar component

---
+++ src/app/[locale]/dashboard/_components/Sidebar/Sidebar.tsx
@@ -0,0 +1,41 @@
+'use client';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import sxStyles from './styles';
+import React from 'react';
+import { useTheme, Theme } from '@mui/material/styles';
+import { useMediaQuery } from '@mui/material';
+import OpenSideBarButton from '../OpenSideBarButton/OpenSideBarButton';
+import NewBoardLink from '@/dashboard/NewBoardLink/NewBoardLink';
+
+export default function Sidebar({
+  children,
+  isSidebarOpen,
+  toggleSideBar,
+}: {
+  children: React.ReactNode;
+  isSidebarOpen: boolean;
+  toggleSideBar: () => void;
+}) {
+  const theme: Theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  return (
+    <Drawer
+      variant={isMobile ? 'temporary' : 'persistent'}
+      open={isSidebarOpen}
+      onClose={toggleSideBar}
+      sx={sxStyles.drawer}
+    >
+      <Box sx={sxStyles.drawerTopBar}>
+        <OpenSideBarButton
+          isSidebarOpen={isSidebarOpen}
+          toggleSidebar={toggleSideBar}
+        />
+        <NewBoardLink />
+      </Box>
+      {/* <Box sx={sxStyles.controls}>{props.savedData}</Box> */}
+      {children}
+    </Drawer>
+  );
+}
---
+++ src/app/[locale]/dashboard/_components/Sidebar/styles.ts
@@ -0,0 +1,53 @@
+import { SxProps } from '@mui/system/styleFunctionSx/styleFunctionSx';
+import theme from '@/theme';
+
+const drawerWidth = 310; // Adjust this value as needed
+const paddings = { xs: 1, md: 2, lg: 3 };
+const borderRadius = 4;
+
+export default {
+  drawer: {
+    flexShrink: 0,
+    '& .MuiDrawer-paper': {
+      width: drawerWidth,
+      boxSizing: 'border-box',
+      backgroundColor: {
+        xs: '#f5f5f5',
+        sm: 'transparent',
+      },
+      borderRight: '0',
+      overflow: 'hidden',
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 2,
+      py: paddings,
+      pl: paddings,
+    },
+  },
+  drawerMobile: {
+    '& .MuiDrawer-paper': {
+      backgroundColor: theme.palette.grey[100],
+    },
+  },
+  drawerTopBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    p: 1,
+    flexShrink: 0,
+    backgroundColor: theme.palette.grey[100],
+    borderRadius,
+  },
+  controls: {
+    minHeight: 0,
+    px: 1,
+    pb: 1,
+    backgroundColor: theme.palette.grey[100],
+    borderRadius,
+  },
+  board: {
+    minHeight: 0,
+    flexGrow: 1,
+  },
+} satisfies Record<string, SxProps>;
