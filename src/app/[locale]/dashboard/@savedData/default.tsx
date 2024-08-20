b4a93681798faa581e620c87f484c14364b9a6cf
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-08-19 22:29:04 -0300

    Relocate history folder

--- src/app/[locale]/dashboard/@savedData/default.tsx
+++ src/app/[locale]/dashboard/@savedData/default.tsx
@@ -1,7 +1,7 @@
 import TabSelector from './TabsSelector';
 import IntlWrapperForAsync from '@/components/IntlWrapperForAsync/IntlWrapperForAsync';
-import History from '../[id]/history/History';
-import SavedBoard from '../[id]/savedBoards/SavedBoard';
+import History from './_components/history/History';
+import SavedBoard from './_components/savedBoards/SavedBoard';
 
 export default async function Default() {
   return (
