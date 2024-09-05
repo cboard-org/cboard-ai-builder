8e149a98bdb29cffea60aa649a7209bbb2cff0a8
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-09-05 17:40:17 -0300

    Upsert board before export

--- src/components/ExportToCboard/ExportToCboard.tsx
+++ src/components/ExportToCboard/ExportToCboard.tsx
@@ -4,11 +4,20 @@ import NorthEast from '@mui/icons-material/NorthEast';
 import { useTranslations } from 'next-intl';
 import { useExportToCboard } from './useExportToCboard';
 import { useBoundStore } from '@/providers/StoreProvider';
+import { useUpsertActualBoard } from '@/dashboard/hooks/useUpsertActualBoard';
+import { BoardRecord } from '@/commonTypes/Board';
 
 export default function ExportToCboard() {
   const message = useTranslations('Board.BoardContainer');
   const board = useBoundStore((state) => state.board);
   const { onExport } = useExportToCboard();
+  const { upsertBoard } = useUpsertActualBoard();
+
+  const handleExport = async (board: BoardRecord) => {
+    const savedBoard = await upsertBoard(board);
+    if (!savedBoard) return;
+    await onExport(savedBoard);
+  };
 
   return (
     <Button
@@ -16,7 +25,7 @@ export default function ExportToCboard() {
       color="primary"
       startIcon={<NorthEast />}
       sx={{ fontSize: '0.7rem' }}
-      onClick={() => board && onExport(board)}
+      onClick={() => board && handleExport(board)}
     >
       {message('exportToCboard')}
     </Button>
