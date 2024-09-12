d49fc02c40ce9c0c7291620c0852fe6f1769bb95
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-09-12 17:21:55 -0300

    Add printTilesRow class

--- src/app/[locale]/dashboard/[id]/@board/Grid/Row/Row.tsx
+++ src/app/[locale]/dashboard/[id]/@board/Grid/Row/Row.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import styles from './Row.module.css';
 
 function Row({ children }: React.PropsWithChildren) {
-  return <div className={styles.root}>{children}</div>;
+  return <div className={`${styles.root} printTilesRow`}>{children}</div>;
 }
 export default Row;
