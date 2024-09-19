f1f34945abe91462b7b346e24841756139a39637
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-09-19 12:29:08 -0300

    Rename NewBoardLink component and update link implementation to use InternalLink

--- src/app/[locale]/dashboard/_components/NewBoardLink/NewBoardLink.tsx
+++ src/app/[locale]/dashboard/_components/NewBoardLink/NewBoardLink.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import styles from './styles';
 import Box from '@mui/material/Box';
-import { INITIAL_CONTENT_ID } from '../constants';
-import { Link } from '@/navigation';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import Tooltip from '@mui/material/Tooltip';
 import { useTranslations } from 'next-intl';
 import { useBoundStore } from '@/providers/StoreProvider';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import InternalLink from '@/components/InternalLink/InternalLink';
 
 function NewBoardLink() {
   const cleanPrompt = useBoundStore((state) => state.cleanPrompt);
   return (
-    <Link onClick={cleanPrompt} href={`/dashboard/${INITIAL_CONTENT_ID}`}>
+    <InternalLink onClick={cleanPrompt} href="/dashboard" prefetch={true}>
       <Box sx={styles.linkContent}>
         <Box sx={styles.smallDevices}>
           <NewBoardIconButton />
@@ -22,7 +21,7 @@ function NewBoardLink() {
           <NewBoardButton />
         </Box>
       </Box>
-    </Link>
+    </InternalLink>
   );
 }
 
