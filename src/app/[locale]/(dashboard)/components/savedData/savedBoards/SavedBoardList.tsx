666b0f32fd47b1f0166a1c1b757dbab7241db47e
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-09-20 20:38:59 -0300

    Refactor SavedBoardsList to support optimistic updates for adding and deleting boards

--- src/app/[locale]/(dashboard)/components/savedData/savedBoards/SavedBoardList.tsx
+++ src/app/[locale]/(dashboard)/components/savedData/savedBoards/SavedBoardList.tsx
@@ -4,21 +4,53 @@ import { useOptimistic } from 'react';
 import { removeSavedBoardsData } from './actions';
 import { SavedBoardsData } from './actions';
 import DataList from '@/components/DataList/DataList';
+import { useEffect } from 'react';
+import { startTransition } from 'react';
+import { getErrorMessage } from '@/common/common';
+
+type Action =
+  | { type: 'ADD_BOARD'; payload: SavedBoardsData }
+  | { type: 'DELETE_BOARD'; payload: SavedBoardsData };
 
 export default function SavedBoardsList({
   initialData,
 }: {
   initialData: SavedBoardsData[];
 }) {
-  const [savedBoards, deletesavedBoard] = useOptimistic(
-    initialData,
-    (savedBoards, savedBoardToDelete: SavedBoardsData) => {
-      return savedBoards.filter((board) => board.id != savedBoardToDelete.id);
-    },
-  );
+  const [savedBoards, updatesavedBoard] = useOptimistic<
+    SavedBoardsData[],
+    Action
+  >(initialData, (savedBoards, action) => {
+    switch (action.type) {
+      case 'ADD_BOARD':
+        return [action.payload, ...savedBoards];
+      case 'DELETE_BOARD':
+        return savedBoards.filter((board) => board.id !== action.payload.id);
+      default:
+        return savedBoards;
+    }
+  });
+
+  //TODO: Fix this useEffect
+  // a hack to update the board list when a new board is added
+  // when the first item is deleted the optimisitic update is not working
+  useEffect(() => {
+    if (initialData[0].id !== savedBoards[0].id) {
+      startTransition(() => {
+        updatesavedBoard({ type: 'ADD_BOARD', payload: initialData[0] });
+      });
+    }
+  }, [initialData, savedBoards, updatesavedBoard]);
+
   const deleteSavedBoardsData = async (savedBoardToDelete: SavedBoardsData) => {
-    deletesavedBoard(savedBoardToDelete);
-    await removeSavedBoardsData(savedBoardToDelete);
+    startTransition(async () => {
+      updatesavedBoard({ type: 'DELETE_BOARD', payload: savedBoardToDelete });
+      try {
+        await removeSavedBoardsData(savedBoardToDelete);
+      } catch (e) {
+        console.error(getErrorMessage(e));
+      }
+    });
   };
 
   return (
