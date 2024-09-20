ef16c0c9efa5cb0e241132a99dc30813ed6ee602
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-09-20 16:14:49 -0300

    Replace Skeleton with custom loading indicator in Loading component

--- src/app/[locale]/(dashboard)/board/[id]/loading.tsx
+++ src/app/[locale]/(dashboard)/board/[id]/loading.tsx
@@ -1,5 +1,26 @@
-import Skeleton from '@mui/material/Skeleton';
+import Box from '@mui/material/Box';
 
 export default function Loading() {
-  return <Skeleton />;
+  return (
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        borderRadius: 1,
+      }}
+    >
+      <Box
+        sx={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>LOADING...</p>
+      </Box>
+    </Box>
+  );
 }
